631bb9e74a0da8388f5f7ab95b9e68292d7b979c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-02-02 11:37:11 +0100

    Make host and port configurable

--- main.py
+++ main.py
@@ -6,21 +6,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello, World!"
+
 
 @app.route('/devices/<device>/up')
 def up(device):
     stick.command(device, "up")
 
+
 @app.route('/devices/<device>/down')
 def down(device):
     stick.command(device, "down")
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Web service for a duofern USB stick.')
     parser.add_argument('--code', help='the pairing code')
+    parser.add_argument('--debug', '-d', help='debug mode', action='store_true')
+    parser.add_argument('--listen', '-l', help='to address to listen on', default='127.0.0.1')
+    parser.add_argument('--port', '-p', help='to port to listen on', default='8080')
     parser.add_argument('--device', help='the USB stick device', default=None)
     args = parser.parse_args(sys.argv[1:])
 
@@ -28,4 +35,4 @@ if __name__ == '__main__':
     stick._initialize()
     stick.start()
 
-    app.run(debug=True)
+    app.run(debug=args.debug is True, host=args.host, port=args.port)
